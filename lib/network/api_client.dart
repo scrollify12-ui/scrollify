d576966f02c31243b64d2530ef24197706b3b98f
Skydreo <skydreo.official@gmail.com>
2026-08-16 19:01:25 +0530

    Configure environment-based API URL

--- lib/network/api_client.dart
+++ lib/network/api_client.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/foundation.dart';
 
 final apiClientProvider = Provider<ApiClient>((ref) {
   return ApiClient();
@@ -9,8 +10,12 @@ class ApiClient {
   late final Dio _dio;
 
   ApiClient() {
+    final String apiUrl = (kReleaseMode || kIsWeb)
+        ? 'https://scrollify-backend.onrender.com/api/'
+        : 'http://192.168.1.5:3000/api/';
+
     _dio = Dio(BaseOptions(
-      baseUrl: 'http://192.168.1.5:3000/api/',
+      baseUrl: apiUrl,
       connectTimeout: const Duration(seconds: 5),
       receiveTimeout: const Duration(seconds: 3),
     ));
